53d4bc4d9465fe917537e9c4ad6e8c645dcf4202
Neminess <73821948+Neminess@users.noreply.github.com>
2021-03-17 21:29:23 +0200

    Koodin siistimistä, kokeiluja

--- scripts/script_sis.js
+++ scripts/script_sis.js
@@ -10,23 +10,24 @@ topnav when the user clicks on the icon */
 }*/
 
 /*Changes the display of the menu*/
+
+/*Korjaan tätä koko ajan, tähän tulee torstaina illalla lisää muutoksia.
+Tän menun saisi tehtyä myös laittamalla eventlistenerin tai lisäämällä html:ään "onresize"-listenerin.
+Testaan sitä lisää torstaina 18.3.*/
+
 function myFunction() {
     var x = document.getElementById("myLinks");
     /*var y= document.getElementById("body");*/
-    if (x.style.display === "block" &&  screen.width < 1200) {
+    if (x.style.display="block" && screen.width<1000) {
       x.style.display = "none";
+      console.log("Feck");
     } 
-    else if (screen.width >=1200){
+    else if (screen.width >=1000){
       x.style.display = "block";
+      console.log("Fuck");
     }
-    else {
-      x.style.display="block";
-    }
-    
+   else {
+     x.style.display="initial";
+     console.log("hell");
+   }
 }
-
-/*  
-    
-    if (y.width >= "1026px")
-    }*/
-
